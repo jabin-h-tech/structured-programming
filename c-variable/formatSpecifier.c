f8278270e05acb715cacf1c7920faa33e9b6d3a0
jabin-h-tech <tanbinsiddikajabin@gmail.com>
2026-09-05 23:55:58 +0600

    Move to formatSpecifier.c inside c-variable folder

--- c-variable/formatSpecifier.c
+++ c-variable/formatSpecifier.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int num = 15;//integer
-    float number = 5.99 ;//float
-    char letter = 'a';//character
+    int num = 15;        // integer
+    float number = 5.99; // float
+    char letter = 'a';   // character
     printf("%d\n", num);
     printf("%f\n", number);
     printf("%c\n", letter);
