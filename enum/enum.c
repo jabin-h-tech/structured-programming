cdb13d69481f0546c2094570468c50940ff51a39
jabin-h-tech <tanbinsiddikajabin@gmail.com>
2026-09-25 17:31:32 +0600

    enum

---
+++ enum/enum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    enum Level
+    {
+        LOW,
+        MEDIUM,
+        HIGH
+    };
+
+    enum Level myVar = MEDIUM;
+
+    printf("%d", myVar);
+
+    return 0;
+}
